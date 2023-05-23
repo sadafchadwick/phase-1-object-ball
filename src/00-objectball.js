5bf156af1bdcfb3e28a583492e534bd823ee5556
Sadaf Chadwick <sadafchadwick@gmail.com>
2023-05-23 15:17:36 -0500

    started player object nesting

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,33 +1,51 @@
 function gameObject(){
    return { 
     home: {
-        teamName: "",
-        colors: [],
-        players: {},
+        teamName: "Brooklyn Nets",
+        colors: ["Black", "White"],
+        players: {
+                "Alan Anderson": {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals:3,
+                    blocks: 1,
+                    slamDunks: 1,
+
+            }, 
+            
+            // "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"],
+            // number: [0, 30, 11, 1, 31],
+            // shoe: [16, 14, 17, 19, 15],
+            // points: [22, 12, 17, 26, 19],
+            // rebounds: [12, 12, 19, 12,2],
+            // assists: [12, 12, 10, 6, 2],
+            // steals:[3, 12, 3, 3, 4],
+            // blocks: [1, 12, 1, 8, 11],
+            // slamDunks: [1, 7, 15, 5, 1],
+        
     },
-    away: {
-        teamName: "",
-        colors: [],
-        players: {},
-    },
-   };
-};
-        /*team:
-            home: {
-                teamName: "Brooklyn Nets",
-                colors: ["black", "white"],
-                players: "c",
-            },
-        };
-        team2 = {
-            away: {
-            teamName: "Charlotte Hornets",
-            colors: ["turquoise", "purple"],
-            players: "f",
-            },
-        };
-    },
-    //console.log('hello world!')
-};
-//const obs = gameObject();
-gameObject();*/
+//     away: {
+//         teamName: "Charlotte Hornets",
+//         colors: ["Turquoise", "Purple"],
+//         players: {
+//             name: ["Jeff Adrien", "Bismak Biyombo", "DeSagna Diop", "Ben Gordon", "Brendan Haywood"],
+//             number: [4, 0, 2, 8, 33],
+//             shoe: [18, 16, 14, 15, 15],
+//             points: [10, 12, 24, 33, 6],
+//             rebounds: [1, 4, 12, 3, 12],
+//             assists: [1, 7, 12, 2, 12],
+//             steals: [2, 7, 4, 1, 22],
+//             blocks: [7, 15, 5, 1, 5],
+//             slamDunks: [2, 10, 5, 0, 12],
+//         },
+//     },
+//    };
+}
+}}
+
+console.log(gameObject());
+
+
