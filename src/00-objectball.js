4d17b0f89d568e308409b6d352659892e8e44e9d
Amelia Freeman <ameliakfreeman@gmail.com>
2023-05-23 14:50:22 -0500

    object skeleton made

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,4 +1,33 @@
 function gameObject(){
-    console.log('hello world!')
-}
-gameObject()
+   return { 
+    home: {
+        teamName: "",
+        colors: [],
+        players: {},
+    },
+    away: {
+        teamName: "",
+        colors: [],
+        players: {},
+    },
+   };
+};
+        /*team:
+            home: {
+                teamName: "Brooklyn Nets",
+                colors: ["black", "white"],
+                players: "c",
+            },
+        };
+        team2 = {
+            away: {
+            teamName: "Charlotte Hornets",
+            colors: ["turquoise", "purple"],
+            players: "f",
+            },
+        };
+    },
+    //console.log('hello world!')
+};
+//const obs = gameObject();
+gameObject();*/
